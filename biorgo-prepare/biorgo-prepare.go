7b9763bdad322ad756a1dc4329b01a8bb7e3a4b6
Anton Davydov <fetsorn@gmail.com>
2020-10-20 21:13:57 +0300

    feat: filter extensions on prepare

--- biorgo-prepare/biorgo-prepare.go
+++ biorgo-prepare/biorgo-prepare.go
@@ -21,29 +21,79 @@ type FilePrepared struct {
 	Path       string `json:"path"`
 	ParsedTime string `json:"parsed_time"`
 	Contents   string `json:"contents"`
-	// UUID       string `json:"uuid"`
 }
 
 // tell if the file has useful data
-func fileIsUseful(path string) bool {
-	return !strings.HasSuffix(path, ".DS_Store")
+func fileIsUseful(path string, ignorePath string) bool {
+
+	extPath := filepath.Ext(path)
+
+	var ignoreStr string
+
+	// when ignore file is not specified, ignore defaults
+	if ignorePath == "empty" {
+		ignoreStr = `
+.DS_Store
+.info
+`
+	} else {
+
+		// read ignore file
+		ignoreFile, err := ioutil.ReadFile(ignorePath)
+		if err != nil {
+			fmt.Println("Failed to read ignore file", err)
+			panic(err)
+		}
+
+		ignoreStr = string(ignoreFile)
+	}
+
+	// split ignore list into array of string
+	ignoreArray := strings.Split(ignoreStr, "\n")
+
+	// check if string equals any of array's elements
+	for ext := range ignoreArray {
+		if ignoreArray[ext] == extPath {
+			return false
+		}
+	}
+
+	return true
 }
 
 // tell if the file is plain text and its contents should be read
 func fileIsText(path string) bool {
-	fileExt := filepath.Ext(path)
-	return fileExt == ".txt" || fileExt == ".org" || fileExt == ".md"
+
+	extPath := filepath.Ext(path)
+
+	textArray := []string{".txt", ".TXT", ".org", ".ORG", ".md", "MD"}
+
+	for ext := range textArray {
+		if textArray[ext] == extPath {
+			return true
+		}
+	}
+
+	return false
+}
+
+func folderIsSkip(path string, info os.FileInfo) bool {
+	return info.Name() == ".git" || strings.Contains(path, "jre/lib")
 }
 
 // prepare files in directory inputPath according to a go template in templatePath, output as desmi to outputPath
-func prepareFiles(inputPath string, templatePath string, outputPath string) {
+func prepareFiles(inputPath string, templatePath string, outputPath string, ignorePath string) {
 
 	jsonArray := []map[string]interface{}{}
 
 	// parse the file into FilePrepared struct, append to array
 	var walkFunction = func(path string, info os.FileInfo, err error) error {
 
-		if !info.IsDir() && fileIsUseful(path) {
+		if info.IsDir() && folderIsSkip(path, info) {
+			return filepath.SkipDir
+		}
+
+		if !info.IsDir() && fileIsUseful(path, ignorePath) {
 			var file FilePrepared
 
 			layout := "<2006-01-02>"
@@ -69,6 +119,12 @@ func prepareFiles(inputPath string, templatePath string, outputPath string) {
 				// legacy solution that imitates previous manual commits
 				re := regexp.MustCompile(`(?m)^\*`)
 				file.Contents = re.ReplaceAllString(file.Contents, "**")
+
+				// replace windows RET with newlines
+				re = regexp.MustCompile("")
+				file.Contents = re.ReplaceAllString(file.Contents, "\n")
+				re = regexp.MustCompile("|")
+				file.Contents = re.ReplaceAllString(file.Contents, " ")
 			}
 
 			jsonStr, _ := json.Marshal(file)
@@ -134,14 +190,17 @@ func main() {
 	var inputPath string
 	var templatePath string
 	var outputPath string
+	var ignorePath string
 
 	// flags declaration using flag package
 	flag.StringVar(&inputPath, "i", "empty", "Please specify storg path")
 	flag.StringVar(&templatePath, "t", "empty", "Please specify template path")
 	flag.StringVar(&outputPath, "o", "empty", "Please specify output path")
+	flag.StringVar(&ignorePath, "e", "empty", "Please specify path to ignore file")
 
 	flag.Parse() // after declaring flags we need to call it
 	// TODO: show usage when no arguments are specified
 
-	prepareFiles(inputPath, templatePath, outputPath)
+	prepareFiles(inputPath, templatePath, outputPath, ignorePath)
+
 }
